4667db3b5192c0ec04a9b45f522790ee41568890
Hgnim <hgn1m@outlook.com>
2026-09-22 18:38:40 +0800

    新增用于调试的页面

---
+++ playground/main.ts
@@ -0,0 +1,4 @@
+import cbcm from '../src/index';
+document.querySelector("#app")!.innerHTML = `
+  <h1>${cbcm()}</h1>
+`;
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export default function () {
     console.log('test');
+    return 'test return'
 }
